e48e28ddcad53b1e679652d692fcd229ac8042f4
jefferson <jefferson.ramos@academico.ifpb.edu.br>
2025-11-11 22:44:22 -0300

    :sparkles:feat: Adiciona Layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,54 @@
+import { AppShell, Group, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import { Footer } from '../components/footer/Footer';
+import { Navbar } from '../components/navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+
+export function Layout() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+        layout='alt'
+        padding="md"
+        navbar={{
+            width: 250,
+            breakpoint: 'sm',
+            collapsed: { mobile: !opened },
+        }}
+        header={{
+            height: 60,
+        }}
+        bg="gray.0" 
+    >
+      {/* Header */}
+      <AppShell.Header withBorder={false}>
+        <Group h="100%" px="md">
+          <Burger 
+            opened={opened} 
+            onClick={toggle} 
+            hiddenFrom="sm" 
+            size="sm" 
+          />
+          <MantineLogo size={25} />
+        </Group>
+      </AppShell.Header>
+
+      {/* Sidebar/Navbar */}
+      <AppShell.Navbar >
+        <Navbar />
+      </AppShell.Navbar>
+
+      {/* Main Content */}
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+
+      {/* Footer */}
+      <AppShell.Footer withBorder={false} >
+        <Footer />
+      </AppShell.Footer>
+    </AppShell>
+  );
+}
