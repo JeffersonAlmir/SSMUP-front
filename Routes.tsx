b741c254472c6d727245cb280d979bd3a38d78bf
jefferson <jefferson.ramos@academico.ifpb.edu.br>
2025-11-11 22:46:52 -0300

    :recycle: refactor: Ajusta rotas principais

--- Routes.tsx
+++ Routes.tsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./src/pages/Inicio";
+import { Layout } from "./src/layout/Layout";
+import Painel from "./src/pages/Painel";
+import Empresas from "./src/pages/Empresas";
+import Alvara from "./src/pages/Alvara";
+import Configuracoes from "./src/pages/Configuracoes";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route element ={<Layout/>}>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/painel" element = {<Painel/>} />
+            <Route path="/empresas" element={<Empresas/>} />
+            <Route path="/alvara" element={<Alvara/>} />
+            <Route path="/configuracoes" element={<Configuracoes/>} />
+            
+        
+        </Route>
       </Routes>
     </BrowserRouter>
   );
