1e8ec84dcba7404800d90ff8990f108df166d86e
jefferson <jefferson.ramos@academico.ifpb.edu.br>
2025-11-11 22:51:00 -0300

    :lipstick: style: Atualiza estilos e estrutura do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,5 @@
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
-import { ActionIcon, Group } from '@mantine/core';
-import { MantineLogo } from '@mantinex/mantine-logo';
+
+import {Text } from '@mantine/core';
 import classes from './Footer.module.css';
 
 
@@ -11,21 +10,12 @@ export function Footer() {
   return (
     <div className={classes.footer}>
       <div className={classes.inner}>
-        <MantineLogo size={28} />
-
-    
-        <Group gap="xs" justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" variant="default" radius="xl">
-            <IconBrandTwitter size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" variant="default" radius="xl">
-            <IconBrandYoutube size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" variant="default" radius="xl">
-            <IconBrandInstagram size={18} stroke={1.5} />
-          </ActionIcon>
-        </Group>
+        <Text c="dimmed" size="sm" className={classes.copyright}>
+          © {new Date().getFullYear()} SSMUP. All rights reserved.
+        </Text>
       </div>
+
+
     </div>
   );
 }
