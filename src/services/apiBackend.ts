8e467495b20cd6b16815e67e26dbd3123252c3a8
dnielbr <everaldodaniel@hotmail.com>
2026-01-28 20:31:46 -0300

    feat: Adiciona um configuração no default.conf do nginx e no service apiBackend.ts, para subir no cluster do laced e resolver erro de CORS.

--- src/services/apiBackend.ts
+++ src/services/apiBackend.ts
@@ -9,3 +9,17 @@ const apiBackend = axios.create({
 });
 
 export default apiBackend;
+
+
+//UTILIZAR ESSA VERSÃO QUANDO FOR SUBIR NO CLUSTER DO LACED
+// import axios from "axios";
+
+// const apiBackend = axios.create({
+//   baseURL: '/v1/api',
+//   timeout: 5000,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// export default apiBackend;
