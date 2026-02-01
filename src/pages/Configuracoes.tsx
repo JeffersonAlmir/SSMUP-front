5fc6db96e321afde81f982d89f1315f7557221ea
jefferson <jefferson.ramos@academico.ifpb.edu.br>
2026-02-01 13:19:14 -0300

    refactor: adiciona componentes  na page de configuração

--- src/pages/Configuracoes.tsx
+++ src/pages/Configuracoes.tsx
@@ -1,8 +1,27 @@
+import { Tabs } from "@mantine/core";
+import { IconFileText, IconUser } from "@tabler/icons-react";
+import FormCadastroFuncionario from "../components/form/FormCadastroFuncionario";
+import { UsersTable } from "../components/table/UsersTable";
+
 const Configuracoes = () =>{
     return (
-        <div>
-            <h3>Page configuracoes</h3>
-        </div>
+        <Tabs defaultValue="lista"  radius="sm">
+            <Tabs.List>
+                <Tabs.Tab value="lista" leftSection={<IconFileText size={16} />}>
+                    Visualizar Funcionários
+                </Tabs.Tab>
+                <Tabs.Tab value="cadastro" leftSection={<IconUser size={16} />}>
+                    Novo Funcionário
+                </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="lista" pt="xs">
+                <UsersTable/>
+            </Tabs.Panel>
+            <Tabs.Panel value="cadastro" pt="xs">
+                <FormCadastroFuncionario/>
+            </Tabs.Panel>
+        </Tabs>
     );
 }
 
