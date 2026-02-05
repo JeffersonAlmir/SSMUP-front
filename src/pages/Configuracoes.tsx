90cc9aa49975ebaf4e0b74e8e2a0ef70915f8274
jefferson <jefferson.ramos@academico.ifpb.edu.br>
2026-02-05 11:49:29 -0300

    clear: limpeza de compone que não esta sendo usado

--- src/pages/Configuracoes.tsx
+++ src/pages/Configuracoes.tsx
@@ -1,27 +1,7 @@
-import { Tabs } from "@mantine/core";
-import { IconFileText, IconUser } from "@tabler/icons-react";
-import FormCadastroFuncionario from "../components/form/FormCadastroFuncionario";
-import { UsersTable } from "../components/table/UsersTable";
 
 const Configuracoes = () =>{
     return (
-        <Tabs defaultValue="lista"  radius="sm">
-            <Tabs.List>
-                <Tabs.Tab value="lista" leftSection={<IconFileText size={16} />}>
-                    Visualizar Funcionários
-                </Tabs.Tab>
-                <Tabs.Tab value="cadastro" leftSection={<IconUser size={16} />}>
-                    Novo Funcionário
-                </Tabs.Tab>
-            </Tabs.List>
-
-            <Tabs.Panel value="lista" pt="xs">
-                <UsersTable/>
-            </Tabs.Panel>
-            <Tabs.Panel value="cadastro" pt="xs">
-                <FormCadastroFuncionario/>
-            </Tabs.Panel>
-        </Tabs>
+        <h1>configuracoes</h1>
     );
 }
 
