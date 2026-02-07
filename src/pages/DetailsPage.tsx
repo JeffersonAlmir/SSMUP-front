0cfadafe5fb3f6818587560b144873f0b74f4299
jefferson <jefferson.ramos@academico.ifpb.edu.br>
2026-02-06 21:20:21 -0300

    refactor: remoção de lógica do componente e colocando dentro do contexto

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,115 +1,14 @@
 import { Button, Card, Container, Divider, Group, Paper, Text, Title } from "@mantine/core";
-import { IconArrowLeft, IconCheck, IconX } from "@tabler/icons-react";
+import { IconArrowLeft} from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import DetailsEmpresa from "../components/details/DetailsEmpresa";
 import { useUpdateEmpresaContext } from "../hooks/useUpdateEmpresaContext";
-import { useEffect, useState } from "react";
-import apiBackend from "../services/apiBackend";
-import { notifications } from "@mantine/notifications";
 import EmpresaActionButton from "../components/buttons/EmpresaActionsButton";
 
 
 const DetailsPage = () => {
-    const [isDisabled, setIsDisabled] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const {dataEmpresa, setDataEmpresa} = useUpdateEmpresaContext()
+    const {dataEmpresa} = useUpdateEmpresaContext();
     const navigate = useNavigate();
-
-    const handleDownloadPDF = async() =>{
-        setLoading(true)
-        try {
-           const response = await apiBackend.post(`/licensas/emitir/${dataEmpresa.id}`,{},{
-            responseType: 'blob',
-           })
-           if(response.status ===200){
-
-                const url = window.URL.createObjectURL(new Blob([response.data]));
-        
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', `licenca-empresa-${dataEmpresa.razaoSocial}.pdf`); 
-                document.body.appendChild(link);
-                link.click();
-
-
-                link.parentNode?.removeChild(link);
-                window.URL.revokeObjectURL(url);
-
-                notifications.show({
-                    title: 'Sucesso!',
-                    message: 'A licença foi gerada e o download iniciado.',
-                    color: 'teal',
-                    icon: <IconCheck size={18} />,
-            });
-            }
-        } catch (error) {
-            console.error("Erro ao emitir:", error);
-            notifications.show({
-                title: 'Erro na emissão',
-                message: 'Não foi possível gerar o PDF. Tente novamente mais tarde.',
-                color: 'red',
-                icon: <IconX size={18} />,
-        });
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    const handleInativar = async () =>{
-        try {
-            const response = await apiBackend.delete(`/empresas/${dataEmpresa.id}/inativar`);
-            if(response.status === 204){
-                setDataEmpresa({ ...dataEmpresa, ativo: false });
-                notifications.show({
-                    title: 'Sucesso!',
-                    message: 'A empresa foi inativada.',
-                    color: 'green',
-                    icon: <IconCheck size={18} 
-                />
-        });
-            }
-        } catch (error) {
-            console.error("Erro ao inativar:", error); 
-            notifications.show({
-                title: 'Erro',
-                message: 'Não foi possível realizar a inativação. Tente novamente.',
-                color: 'red',
-                icon: <IconX size={18} />
-            }); 
-        }
-    }
-    const handleAtivar = async () =>{
-        try {
-            const response = await apiBackend.put(`/empresas/${dataEmpresa.id}/ativar`);
-            if(response.status === 204){
-                setDataEmpresa({ ...dataEmpresa, ativo: true });
-                notifications.show({
-                    title: 'Sucesso!',
-                    message: 'A empresa foi ativada.',
-                    color: 'green',
-                    icon: <IconCheck size={18} 
-                />
-        });
-            }
-        } catch (error) {
-            console.error("Erro ao inativar:", error); 
-            notifications.show({
-                title: 'Erro',
-                message: 'Não foi possível realizar a ativação. Tente novamente.',
-                color: 'red',
-                icon: <IconX size={18} />
-            }); 
-        }
-    }
-
-    useEffect(() => {
-        const fechDisableButtom = () =>{
-            if (dataEmpresa.cnae.risco === "RISCO_III_ALTO"){
-                setIsDisabled(true);
-            }
-        }
-        fechDisableButtom();
-    },[dataEmpresa])
  
     if (!dataEmpresa) {
         return (
@@ -141,14 +40,7 @@ const DetailsPage = () => {
                 </Group>
                 <Group> 
 
-                    <EmpresaActionButton
-                        dataEmpresa={dataEmpresa}
-                        loading={loading}
-                        isDisabled={isDisabled}
-                        handleSubmit={handleDownloadPDF}
-                        handleInativar={handleInativar}
-                        handleAtivar={handleAtivar}
-                    />
+                    <EmpresaActionButton/>
                   
                 </Group>
             </Group>
